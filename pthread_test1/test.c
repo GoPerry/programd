ff69238b5f2faa610ce7fa7f352a33e9a5d1b8e5
yuzaipiaofei <yuanwei2088@gmail.com>
2012-03-03 09:46:07 -0800

    test1 pass

--- pthread_test1/test.c
+++ pthread_test1/test.c
@@ -2,24 +2,35 @@
 #include "stdio.h"
 #include "pthread.h"
 #include "stdlib.h"
-
+// qiang zhi lei xing zhuan huan wei void * 
+// pthread _test is original pointer 
 void *pthread_test()
 {
-
-	while(1)
+	int i=5;
+	while(i--)
 	{
 		printf("i am pthread\n");
 
 	}
 }
 int main()
-{	phread_t pid;
-	pthread_create(&pid,NULL,pthread_test,NULL);
-	while(1)
+{	pthread_t pid1;
+	int err;
+	pthread_create(&pid1,NULL,pthread_test,NULL);
+	if(err!=0)
+		printf("cant not create test pthread!");
+	pthread_join(pid1,NULL);
+	if(err!=0)
+		printf("can not join test pthread !");
+	
+	
+	int x=3;
+	while(x--)
 	{
 	printf("i am main pthread!!!\n");
 	
 	}
 return 0;
+//exit(0);
 
 }
